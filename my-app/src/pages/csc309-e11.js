e250c4dd1085837c49006e233e145896d389265d
Christoffer Tan <tanchristoffer@gmail.com>
2024-11-23 15:40:59 -0500

    done

---
+++ my-app/src/pages/csc309-e11.js
@@ -0,0 +1,7 @@
+export default function AutoTester() {
+    return (
+        <div id="auto-tester">
+            Hi. I am tanchr23, and I have learned how to develop a full-stack web app and deploy it!
+        </div>
+    );
+}
