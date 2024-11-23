fb8b230c8368023d809a018a591a65490576a5a2
Christoffer Tan <tanchristoffer@gmail.com>
2024-11-23 15:45:47 -0500

    edit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function getBaseUrl() {
+    return ""; // Replace with your actual deployed URL
+}
+
+function getUTorId() {
+    return "tanchr23"; // Replace with your UTorId
+}
+
+module.exports = {
+    getBaseUrl,
+    getUTorId
+};
